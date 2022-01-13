95eecc4dc8dfc541ad600decd9280467fde09404
carlosalfarodev <rgkalin@gmail.com>
2022-01-12 18:53:49 -0800

    Adds testing pages, layout, and components

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      Footer{' '}
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Developed by Carlos Alfaro
+      </a>
+    </footer>
+  );
+}
